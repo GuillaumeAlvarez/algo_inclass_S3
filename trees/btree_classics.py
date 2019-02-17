3bf74b8031eec5a2d9b513901109c8854a8c5cbc
Nathalie <nathalie.bouquet@epita.fr>
2019-02-17 19:27:18 +0100

    Add files via upload

--- trees/btree_classics.py
+++ trees/btree_classics.py
@@ -18,15 +18,22 @@ def minBTree(B):
     return B.keys[0]
 
 def maxBTree(B):
-    if B.children is None:
+    if B.children == []:
         return B.keys[-1]
     else:
         return maxBTree(B.children[-1])
     
 def __binarySearchPos(L, x, left, right):
-    # FIXME
-    pass
-
+    if left == right:
+        return left
+    else:
+        mid = left + (right - left) // 2
+        if x == L[mid]:
+            return mid
+        elif x < L[mid]:
+            return __binarySearchPos(L, x, left, mid)
+        else:
+            return __binarySearchPos(L, x, mid+1, right)
 
 def binarySearchPos(L, x):
     """
@@ -34,15 +41,48 @@ def binarySearchPos(L, x):
     """
     return __binarySearchPos(L, x, 0, len(L))
 
-
 def __searchBtree(B, x):
-    #FIXME
-    pass
-
+    i = binarySearchPos(B.keys, x)
+    if i < B.nbkeys and B.keys[i] == x:
+        return (B, i)
+    else:
+        if B.children == []:
+            return None
+        else:
+            return __searchBtree(B.children[i], x)
 
 def searchBTree(B, x):
     return None if not B else __searchBtree(B, x)
 
+
+# 2.3 insertion
+
+def split(B, i):
+    '''
+    splits the child i of B
+    conditions:
+    - B is a nonempty tree and its root is not a 2t-node.
+    - The child i of B exists and its root is a 2t-node.
+    '''
+
+    #FIXME
+    pass
+
+def __insert(B, x):
+    '''
+    conditions:
+    - B is a nonempty tree 
+    - its root is not a 2t-node
+    '''
+    #FIXME
+    pass
+
+def insert(B, x):
+    if B == None:
+        return btree.BTree([x])
+    else:
+        #FIXME
+        pass
 
 
     
